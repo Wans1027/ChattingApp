366ba813d0269955a1d3efc2e4589c69c99b9102
wan1027 <rladhkstmd10@daum.net>
2023-06-14 16:53:53 +0900

    feat: Get List in ChatPartService

--- src/main/java/Chat/chattingApp/service/ChatPartService.java
+++ src/main/java/Chat/chattingApp/service/ChatPartService.java
@@ -1,10 +1,14 @@
 package Chat.chattingApp.service;
 
+import Chat.chattingApp.entity.ChattingRoom;
+import Chat.chattingApp.entity.Member;
 import Chat.chattingApp.repository.ChatPartRepository;
 import Chat.chattingApp.repository.ChattingRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -14,11 +18,15 @@ public class ChatPartService {
     private final ChatPartRepository chatPartRepository;
 
     /**
-     * 채팅방에 누가있는지
+     * 한 채팅방에 포함된 유저들
      * 한유저가 보유하고 있는 채팅방 목록
      */
-    public void getListInChattingRoom(Long chattingRoomId) {
+    public List<Member> getMemberListInChattingRoom(Long chattingRoomId) {
+        return chatPartRepository.findMemberListInChatRoom(chattingRoomId);
+    }
 
+    public List<ChattingRoom> getChattingRoomListInUsers(Long memberId) {
+        return chatPartRepository.findChatRoomListByUser(memberId);
     }
 
 }
