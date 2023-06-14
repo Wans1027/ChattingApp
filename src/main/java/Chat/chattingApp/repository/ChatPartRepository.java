f16b1bd8672cc07e34ba5c8a3242c8a208993116
wan1027 <rladhkstmd10@daum.net>
2023-06-14 16:47:32 +0900

    feat: Search GroupByUser And UsersByGruop In ChatPartRepo

--- src/main/java/Chat/chattingApp/repository/ChatPartRepository.java
+++ src/main/java/Chat/chattingApp/repository/ChatPartRepository.java
@@ -1,12 +1,28 @@
 package Chat.chattingApp.repository;
 
 import Chat.chattingApp.entity.ChatParticipation;
+import Chat.chattingApp.entity.ChattingRoom;
+import Chat.chattingApp.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatPartRepository extends JpaRepository<ChatParticipation, Long> {
 
     @Override
     Optional<ChatParticipation> findById(Long aLong);
+
+    @EntityGraph(attributePaths = {"member", "chattingRoom"})
+    @Query("select c.member from ChatParticipation c where c.chattingRoom.id = :roomId")
+    List<Member> findMemberListInChatRoom(@Param("roomId") Long roomId);
+
+    @EntityGraph(attributePaths = {"member", "chattingRoom"})
+    @Query("select c.chattingRoom from ChatParticipation c where c.member.id = :memberId")
+    List<ChattingRoom> findChatRoomListByUser(@Param("roomId") Long memberId);
+
+
 }
--- src/main/java/Chat/chattingApp/service/ChatPartService.java
+++ src/main/java/Chat/chattingApp/service/ChatPartService.java
@@ -1,5 +1,6 @@
 package Chat.chattingApp.service;
 
+import Chat.chattingApp.repository.ChatPartRepository;
 import Chat.chattingApp.repository.ChattingRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChatPartService {
 
-    private final ChattingRoomRepository chattingRoomRepository;
+    private final ChatPartRepository chatPartRepository;
+
+    /**
+     * 채팅방에 누가있는지
+     * 한유저가 보유하고 있는 채팅방 목록
+     */
+    public void getListInChattingRoom(Long chattingRoomId) {
+
+    }
 
 }
