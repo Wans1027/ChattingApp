6d181fc8f318b6d5824b8cd1a61159aa641cd36e
wan1027 <rladhkstmd10@daum.net>
2023-06-14 16:28:04 +0900

    feat: 메세지 보내기

--- src/main/java/Chat/chattingApp/repository/MessageRepository.java
+++ src/main/java/Chat/chattingApp/repository/MessageRepository.java
@@ -2,6 +2,14 @@ package Chat.chattingApp.repository;
 
 import Chat.chattingApp.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    @Query("select min(m.messageId) from Message m where m.chattingRoomId = :roomId")
+    Optional<Long> findMinIdInGroup(@Param("roomId") Long roomId);
+
 }
--- src/main/java/Chat/chattingApp/service/MessageService.java
+++ src/main/java/Chat/chattingApp/service/MessageService.java
@@ -1,6 +1,8 @@
 package Chat.chattingApp.service;
 
+import Chat.chattingApp.entity.Message;
 import Chat.chattingApp.repository.MemberRepository;
+import Chat.chattingApp.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MessageService {
 
-    private final MemberRepository memberRepository;
+    private final MessageRepository messageRepository;
 
+    public void sendMessage(Long roomId, String detailMessage, Long senderId) {
+        Long messageId = messageRepository.findMinIdInGroup(roomId).orElse(-1L);
+        Message message = new Message(roomId, messageId + 1, detailMessage);
+        messageRepository.save(message);
+    }
 
 }
