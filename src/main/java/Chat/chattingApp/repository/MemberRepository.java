8be7ebe3269423d320b40f913deee58fe98f2f36
wan1027 <rladhkstmd10@daum.net>
2023-06-14 14:47:31 +0900

    feat: Add findByNameAndEmail in MemberRepo

--- src/main/java/Chat/chattingApp/repository/MemberRepository.java
+++ src/main/java/Chat/chattingApp/repository/MemberRepository.java
@@ -2,6 +2,9 @@ package Chat.chattingApp.repository;
 
 import Chat.chattingApp.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,6 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Override
     Optional<Member> findById(Long aLong);
 
-
+    @Transactional(readOnly = true)
+    @Query("select m from Member m where m.name = :name and m.email = :email")
+    Optional<Member> findByNameAndEmail(@Param("name")String name, @Param("email") String email);
 
 }
