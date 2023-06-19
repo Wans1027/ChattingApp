0d477e9fd739f22a3ecbe941f2a337de3b8fb811
Wan1027 <rladhkstmd10@daum.net>
2023-06-19 23:30:42 +0900

    feat: Message컨트롤러 SendMessage Stomp를 이용해 메세지를 보냄

--- src/main/java/Chat/chattingApp/controller/MessageController.java
+++ src/main/java/Chat/chattingApp/controller/MessageController.java
@@ -4,6 +4,8 @@ import Chat.chattingApp.service.MessageService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,18 @@ public class MessageController {
 
     @PostMapping("/sendMessage")
     public void sendMessage(@RequestBody SendMessage request){
+        //DB 저장로직
         messageService.sendMessage(request.roomId, request.detailMessage, request.senderId);
+    }
+
+    private final SimpMessageSendingOperations sendingOperations;
+
+    @MessageMapping("/chat/message")
+    public void enter(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setMessage(message.getSender()+"님이 입장하였습니다.");
+        }
+        sendingOperations.convertAndSend("/topic/chat/room/"+message.getRoomId(),message);
     }
 
 
