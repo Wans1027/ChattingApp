50b2736c34eb4587ba97ed8aa40722887e19bc60
Wan1027 <rladhkstmd10@daum.net>
2023-07-03 21:19:45 +0900

    refactor: Change Cache Queue to Map<roomId, Queue<Message>> & create Destroy Method

--- src/main/java/Chat/chattingApp/service/MessageService.java
+++ src/main/java/Chat/chattingApp/service/MessageService.java
@@ -6,18 +6,21 @@ import Chat.chattingApp.repository.MemberRepository;
 import Chat.chattingApp.repository.MessageRepository;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.Type;
 import java.util.*;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MessageService {
+public class MessageService implements DisposableBean {
 
     private final MessageRepository messageRepository;
-    private static final Queue<Message> messageQueue = new LinkedList<>();
+    //private static final Queue<Message> messageQueue = new LinkedList<>();
+    private static Map<Long, Queue<Message>> messageMap = new HashMap<>();
     private final EntityManager em;
     private static final int messageQueueSize = 5;
 
@@ -32,18 +35,49 @@ public class MessageService {
 
     public void saveMessage(Message.MessageType type, Long roomId, String detailMessage, Long senderId) {
 
-        Message message = new Message(Message.MessageType.TALK, roomId, senderId, detailMessage);
-        messageQueue.add(message);
-        if(messageQueue.size() == messageQueueSize) commitMessageQueue();
+        Message message = new Message(type, roomId, senderId, detailMessage);
+        //messageQueue.add(message);
+
+        if(!messageMap.containsKey(roomId)){
+            Queue<Message> q = new LinkedList<>();
+            q.add(message);
+            messageMap.put(roomId, q);
+        }
+        else {
+            Queue<Message> mQueue = messageMap.get(roomId);
+            mQueue.add(message);
+            if(mQueue.size() > 50){
+                Queue<Message> q = new LinkedList<>();
+                for (int i = 0; i < 20; i++) {
+                    q.add(mQueue.poll());
+                }
+                commitMessageQueue(q);
+            }
+            messageMap.put(roomId, mQueue);
+
+            //큐에 저장된걸 DB 에 언제 넘겨줘야 할까???
+            /*
+             * 큐가 50개 쌓이면 그중 20개 DB행
+             *
+             * 메세지를 가져올 때
+             * 메모리를 먼저 조회
+             * 없다면 DB를 조회
+             * 서버를 종료할 때 메모리에 있는 데이터를 DB로 commit
+             */
+        }
+        
+        
+        
+        //if(messageQueue.size() == messageQueueSize) commitMessageQueue();
     }
 
     public List<Message> getMessages(Long roomId) {
         return messageRepository.findMessageInChattingRoom(roomId);
     }
 
-    public void commitMessageQueue() {
+    public void commitMessageQueue(Queue<Message> messageQueue) {
         //쓰기 지연
-        for (int i = 0; i < messageQueueSize; i++) {
+        for (int i = 0; i < messageQueue.size(); i++) {
             Message message = messageQueue.poll();
             em.persist(message);
 
@@ -51,4 +85,11 @@ public class MessageService {
         em.flush();
     }
 
+    @Override
+    public void destroy() throws Exception {
+        //MessageService Bean 이 삭제될때 즉 서버가 shutDown 될때 동작
+        for (Queue<Message> messages : messageMap.values()) {
+            commitMessageQueue(messages);
+        }
+    }
 }
