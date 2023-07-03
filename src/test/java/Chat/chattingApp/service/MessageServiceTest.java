601453f3245b996978ecac5dc254ac4532db7e47
Wan1027 <rladhkstmd10@daum.net>
2023-07-03 22:27:43 +0900

    test: DB & Cache 성능차이 테스트

--- src/test/java/Chat/chattingApp/service/MessageServiceTest.java
+++ src/test/java/Chat/chattingApp/service/MessageServiceTest.java
@@ -2,10 +2,13 @@ package Chat.chattingApp.service;
 
 import Chat.chattingApp.entity.Message;
 import Chat.chattingApp.repository.MessageRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
 
 import static Chat.chattingApp.entity.Message.MessageType.ENTER;
 import static Chat.chattingApp.entity.Message.MessageType.TALK;
@@ -14,6 +17,10 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class MessageServiceTest {
+    @BeforeEach
+    public void setUp() {
+        stopWatch = new StopWatch();
+    }
 
     @Autowired
     MessageRepository messageRepository;
@@ -21,15 +28,26 @@ class MessageServiceTest {
     @Autowired
     MessageService messageService;
 
+    private StopWatch stopWatch;
+
+
     @Test
-    void saveMessage() {
+    void 실행시간() {
         for (int i = 0; i < 30; i++) {
+            //DB 저장
             messageRepository.save(new Message(TALK, 1L,1L,"Message"));
-        }
-        for (int i = 0; i < 30; i++) {
+            //Memory 저장
             messageService.saveMessage(TALK, 1L, "Message", 1L);
         }
 
+        stopWatch.start("DB");
+        messageService.getMessages(1L);
+        stopWatch.stop();
 
+        stopWatch.start("Cache");
+        messageService.getMessagesInCache(1L);
+        stopWatch.stop();
+
+        System.out.println(stopWatch.prettyPrint());
     }
 }
