057877d1a09bdbb058adcb11d740e68081195321
Wan1027 <rladhkstmd10@daum.net>
2023-07-10 16:58:48 +0900

    fix: RedisConfig Error 해결

--- src/main/java/Chat/chattingApp/redis/RedisConfig.java
+++ src/main/java/Chat/chattingApp/redis/RedisConfig.java
@@ -1,5 +1,6 @@
 package Chat.chattingApp.redis;
 
+import Chat.chattingApp.entity.Message;
 import org.springframework.beans.factory.annotation.Value;
 
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,6 +11,8 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.LinkedList;
 
 @EnableCaching
 @Configuration
@@ -27,10 +30,10 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, LinkedList<Message>> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
         // Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(ChattingDto.class);
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<String, LinkedList<Message>> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         // redisTemplate.setValueSerializer(new StringRedisSerializer());
