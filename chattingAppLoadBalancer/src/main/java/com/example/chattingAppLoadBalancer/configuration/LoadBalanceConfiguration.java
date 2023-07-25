c4dbe6712e7eae9c4b255020911eb9e2ad0eb248
Wan1027 <rladhkstmd10@daum.net>
2023-07-26 00:08:48 +0900

    Gradle: imple Cloud for loadbalance

--- chattingAppLoadBalancer/src/main/java/com/example/chattingAppLoadBalancer/configuration/LoadBalanceConfiguration.java
+++ chattingAppLoadBalancer/src/main/java/com/example/chattingAppLoadBalancer/configuration/LoadBalanceConfiguration.java
@@ -1,4 +1,15 @@
 package com.example.chattingAppLoadBalancer.configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
+import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
 public class LoadBalanceConfiguration {
 }
