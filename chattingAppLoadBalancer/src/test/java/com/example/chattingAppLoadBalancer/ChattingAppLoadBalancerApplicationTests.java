0895ad2f5c984a148b6adedee2ca0c2dae44bfed
Wan1027 <rladhkstmd10@daum.net>
2023-07-25 23:44:37 +0900

    AddFile: 채팅앱 로드밸런싱용 서버

---
+++ chattingAppLoadBalancer/src/test/java/com/example/chattingAppLoadBalancer/ChattingAppLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattingAppLoadBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingAppLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
