62552f74d6fb8ddbd39237b791d937b6ff7b1bbd
Wan1027 <rladhkstmd10@daum.net>
2023-07-26 00:20:01 +0900

    feat: LoadBalancing configuration

--- chattingAppLoadBalancer/src/main/java/chattingAppLoadBalancer/ChattingAppLoadBalancerApplication.java
+++ chattingAppLoadBalancer/src/main/java/chattingAppLoadBalancer/ChattingAppLoadBalancerApplication.java
@@ -1,4 +1,4 @@
-package com.example.chattingAppLoadBalancer;
+package chattingAppLoadBalancer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
