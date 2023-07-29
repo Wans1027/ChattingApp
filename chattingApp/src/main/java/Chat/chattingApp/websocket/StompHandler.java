1665323ecc68f7af750ada9d3cf1916587904364
Wan1027 <rladhkstmd10@daum.net>
2023-07-29 15:52:59 +0900

    feat: WebSocket EventListener

--- chattingApp/src/main/java/Chat/chattingApp/websocket/StompHandler.java
+++ chattingApp/src/main/java/Chat/chattingApp/websocket/StompHandler.java
@@ -2,10 +2,15 @@ package Chat.chattingApp.websocket;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Arrays;
 
 
 @RequiredArgsConstructor
@@ -18,5 +23,15 @@ public class StompHandler implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         //log.info("Stomp Handler 실행");
         return message;
+    }
+
+    @EventListener
+    public void handleWebSocketConnectionListener(SessionConnectedEvent event){
+        log.info("사용자 입장");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectionListener(SessionDisconnectEvent event){
+        log.info("사용자 퇴장");
     }
 }
