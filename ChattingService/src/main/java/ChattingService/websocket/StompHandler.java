1d1303b0a5c6f375c0afa84464b938fb6d1429f1
Wan1027 <rladhkstmd10@daum.net>
2023-08-12 11:52:54 +0900

    feat: Add Exception

---
+++ ChattingService/src/main/java/ChattingService/websocket/StompHandler.java
@@ -0,0 +1,41 @@
+package ChattingService.websocket;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.security.Principal;
+
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class StompHandler implements ChannelInterceptor {
+
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        //log.info("Stomp Handler 실행");
+        return message;
+    }
+
+    @EventListener
+    public void handleWebSocketConnectionListener(SessionConnectedEvent event){
+        log.info("사용자 입장");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectionListener(SessionDisconnectEvent event){
+        log.info("사용자 퇴장");
+    }
+}
